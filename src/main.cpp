859b31a9727b33e1d23fa302c45e3d21fe6cf591
Shannon <shannonmvoth@gmail.com>
2024-04-12 16:47:18 -0600

    runs, but laggy. needs more testing

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,10 @@
 #include <cmath>
 #include "NostromoPinsAndGlobals.h"
 #include "SGUltrasonic.h"
+#include <vector>
+#include <ESP32Servo.h>
+
+using namespace std;
 
 // Right motor side
 #define RSmotorPin1 32 // Riv: IN1_PIN
@@ -26,8 +30,8 @@ const int RSpwmChannel = 0;
 const int LSpwmChannel = 0;
 const int resolution = 8;
 const int run_time = 2000; // Riv: Duration
-int Lspeed = 255;          // Riv: straight_A
-int Rspeed = 255;          // Riv: straight_B
+int Lspeed = 100;          // Riv: straight_A
+int Rspeed = 100;          // Riv: straight_B
 
 // Accelerometer (IMU) LIS3DH variables
 Adafruit_LIS3DH lis = Adafruit_LIS3DH();
@@ -36,8 +40,8 @@ float moving_x1 = 0;
 float moving_x2 = 0;
 float moving_y1 = 0;
 float moving_y2 = 0;
-const float xmoved = .2;
-const float ymoved = .2;
+const float xmoved = .1;
+const float ymoved = .1;
 float heading = 0;
 
 // compass variables (this is based on location)
@@ -49,9 +53,35 @@ const float declinationAngle = 7.6666666667; // Westminster
 
 // other globals
 const byte numChars = 32;
-char receivedChars[numChars];
+// char receivedChars[numChars];
+int receivedChars = 0;
 boolean newData = false;
 float ultimate_direction = 0;
+
+// Data Structures
+SparkFun_VL53L5CX myImager;
+VL53L5CX_ResultsData measurementData; // Result data class structure, 1356 bytes of RAM, Used to store distances
+Servo rotationalServo;                // Creates servo objects
+Servo pitchServo;
+vector<int> detectedObjectVector = {};
+vector<int> correspondingDistanceVector = {};
+
+// ToF Servo Assembly Variables
+int rotServoPin = 4;
+int pitchServoPin = 5;
+
+// Universal Variables
+int imageResolution = 0; // Used to pretty print output
+int imageWidth = 0;      // Used to pretty print output
+int minDistance = 150;   // Set nearest distance an object can get to the sensor (mm)
+int height1 = 128;       // Know height of the ToF sensor off the ground on a flat plane (mm)
+int zone1Locations[24] = {0, 1, 2, 8, 9, 10, 16, 17, 18, 24, 25, 26, 32, 33, 34, 40, 41, 42, 48, 49, 50, 56, 57, 58};
+int zone2Locations[16] = {3, 4, 11, 12, 19, 20, 27, 28, 35, 36, 43, 44, 51, 52, 59, 60};
+int zone3Locations[24] = {5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 61, 62, 63};
+int zone1Values[24] = {};
+int zone2Values[16] = {};
+int zone3Values[24] = {};
+int which_way = 0;
 
 // put function declarations here:
 void Forward();
@@ -66,8 +96,20 @@ void cardinalDir(float headingDegrees);
 float facing_direction();
 void get_input();
 void show_input();
-void do_turn(char receivedChars[numChars]);
+// void do_turn(char receivedChars[numChars]);
+void do_turn(int receivedChars);
 int round_to_15(float temp);
+void remindTheSensorItExists();
+void setup_servos();
+void servo_sweep();
+void sensorIntitiation();
+bool dataProcessed();
+bool objectDetection();
+int heightCalculator(int smol);
+int getDistance();
+int objectAvoidance();
+void get_input();
+void get_turn_info();
 
 void setup() // put your setup code here, to run once:
 {
@@ -95,8 +137,9 @@ void setup() // put your setup code here, to run once:
   delay(5000);
   IMU_Setup();
   Compass_Setup();
-  // for Testing
-  // Serial.println("Testing DC Motor...");
+  setup_servos();
+  sensorIntitiation();
+  servo_sweep();
 }
 
 //
@@ -113,9 +156,10 @@ void loop()
   if (stop_or_go == 1)
   {
     Stop();
-    Serial.print("Turn in degrees, left is negative, right positive: ");
-    get_input();
-    show_input();
+    // Serial.print("Turn in degrees, left is negative, right positive: ");
+    // get_input();
+    // show_input();
+    get_turn_info();
     delay(100);
     do_turn(receivedChars);
     //  are_we_moving();
@@ -125,11 +169,11 @@ void loop()
   {
     Serial.println("Go");
     Forward();
-    // are_we_moving();
+    are_we_moving();
     //  delay(100);        //only for testing
   }
   // Serial.println("loop end");
-  Serial.println("");
+  //Serial.println("");
 }
 
 //
@@ -278,7 +322,7 @@ bool are_we_moving()
   // Serial.print(event.acceleration.z);
   // Serial.println(" m/s^2 ");
 
-  // delay(200);
+   delay(200);
   lis.getEvent(&event);
 
   moving_x2 = event.acceleration.x;
@@ -290,17 +334,30 @@ bool are_we_moving()
 
   if (abs(moving_x2 - moving_x1) > xmoved) // or (moving_x2 - moving_x1 < -xmoved))
   {
-    Serial.println("moving in X");
+    Serial.print("moving in X: ");
+    //Serial.print(moving_x2);
+    //Serial.print(" - ");
+    //Serial.print(moving_x1);
+    //Serial.print(" = ");
+    //Serial.println(moving_x2 - moving_x1);
     return true;
   }
   else if (abs(moving_y2 - moving_y1) > ymoved) // or (moving_y2 - moving_y1 < -ymoved))
   {
-    Serial.println("moving in Y");
+    Serial.print("moving in Y");
+    //Serial.print(moving_y2);
+    //Serial.print(" - ");
+    //Serial.print(moving_y1);
+    //Serial.print(" = ");
+    //Serial.println(moving_y2 - moving_y1);
     return true;
   }
   else
   {
     Serial.println("NOT MOVING");
+    Stop();
+    Reverse(1000);
+    do_turn(receivedChars);
     return false;
   }
 }
@@ -444,7 +501,7 @@ float facing_direction()
 
 void get_input()
 {
-  static byte ndx = 0;
+ /* static byte ndx = 0;
   char endMarker = '\n';
   char rc;
 
@@ -468,6 +525,7 @@ void get_input()
     Serial.print(" newData: ");
     Serial.print(newData);
     */
+    /*
     if (rc != endMarker)
     {
       receivedChars[ndx] = rc;
@@ -490,6 +548,7 @@ void get_input()
       newData = true;
     }
   }
+  */
 }
 
 //
@@ -509,17 +568,19 @@ void show_input()
 //
 // left is negative, right is positive.
 
-void do_turn(char receivedChars[])
+// void do_turn(char receivedChars[])
+void do_turn(int receivedChars)
 {
-  int val = atoi(receivedChars);
+  //int val = atoi(receivedChars);
+  int val = receivedChars;
   if (val > 90 || val < -90)
   {
     Serial.println("Outside allowable range.");
     return;
   }
-  val = round_to_15(val);
-  Serial.print("do_turn val (rounded to nearest 15): ");
-  Serial.println(val);
+  //val = round_to_15(val);
+  //Serial.print("do_turn val (rounded to nearest 15): ");
+  //Serial.println(val);
   heading = facing_direction();
   int new_heading = heading + val;
 
@@ -535,7 +596,13 @@ void do_turn(char receivedChars[])
     {
       RotateLeft();
       heading = facing_direction();
+      delay(200);
     } while (heading > new_heading);
+  }
+  else if (val == 0)
+  {
+    Forward();
+    heading = facing_direction();
   }
   else
   {
@@ -543,6 +610,7 @@ void do_turn(char receivedChars[])
     {
       RotateRight();
       heading = facing_direction();
+      delay(200);
     } while (heading < new_heading);
   }
 }
@@ -564,4 +632,377 @@ int round_to_15(float temp)
     return -(abs(num_to_fix) - remainder);
   else
     return num_to_fix + multiple - remainder;
+}
+
+//
+//
+// Flip the sensor upwards to see a close neutral surface to help reset the zone values before taking data
+
+void remindTheSensorItExists()
+{
+  delay(500);
+
+  // Cycle Servo up and dwn 3 times
+  for (int i = 0; i <= 3; ++i)
+  {
+    pitchServo.write(5);
+    delay(1000);
+    pitchServo.write(90);
+    delay(1000);
+  }
+}
+
+//
+//
+//
+
+void setup_servos()
+{
+  // Sets up servo objects to be used in the rest of the code
+  // rotationalServo.setPeriodHertz(50);
+  // pitchServo.setPeriodHertz(50);
+
+  rotationalServo.attach(rotServoPin); //, 500, 2400);
+  pitchServo.attach(pitchServoPin);    //, 500, 2400);
+
+  // rotationalServo.write(90);
+  //  pitchServo.write(90);
+
+  // Serial.println("Set Servos to 90");
+}
+
+//
+//
+//
+
+void servo_sweep()
+{
+  rotationalServo.write(45);
+  delay(200);
+  rotationalServo.write(60);
+  delay(200);
+  rotationalServo.write(75);
+  delay(200);
+  rotationalServo.write(90);
+  delay(200);
+  rotationalServo.write(105);
+  delay(200);
+  rotationalServo.write(120);
+  delay(200);
+  pitchServo.write(10);
+  delay(200);
+  pitchServo.write(45);
+  delay(200);
+  pitchServo.write(60);
+  delay(200);
+  pitchServo.write(75);
+  delay(200);
+  pitchServo.write(90);
+  delay(200);
+  rotationalServo.write(90);
+  delay(200);
+}
+
+//
+//
+// Function to intitiat ToF Sensor Settings so I can Clean my code a bit and make it easier to copy and paste
+void sensorIntitiation()
+{
+  delay(1000);
+  Serial.println("SparkFun VL53L5CX Imager Example");
+
+  Wire.begin();          // This resets to 100kHz I2C
+  Wire.setClock(400000); // Sensor has max I2C freq of 400kHz
+
+  Serial.println("Initializing sensor board. This can take up to 10s. Please wait.");
+  if (myImager.begin() == false)
+  {
+    Serial.println(F("Sensor not found - check your wiring. Freezing"));
+    while (1)
+      ;
+  }
+
+  myImager.setResolution(8 * 8); // Enable all 64 pads
+
+  imageResolution = myImager.getResolution(); // Query sensor for current resolution - either 4x4 or 8x8
+  imageWidth = sqrt(imageResolution);         // Calculate printing width
+
+  // The sensor starts collecting data
+  myImager.startRanging();
+}
+
+//
+//
+// checks to see if data can be collected from sensor and if true reads the data into an array
+bool dataProcessed()
+{
+  Serial.println("Is data processed?");
+  delay(500);
+  // Checks to see if the TOF sensor has data prepared to be outputed
+  if (myImager.isDataReady() == true)
+  {
+    if (myImager.getRangingData(&measurementData)) // Read distance data into array
+    {
+      return true;
+    }
+  }
+  Serial.println("NO data processed");
+  delay(500);
+  return false;
+}
+
+//
+//
+// Checks to see if an object has come within the min detect distance
+bool objectDetection()
+{
+  Serial.println("In object detection function");
+  delay(200);
+  for (int y = 0; y <= imageWidth * (imageWidth - 1); y += imageWidth)
+  {
+    for (int x = imageWidth - 1; x >= 0; x--)
+    {
+      // Rejects the values from all of the corner zones when doing general object detection
+      switch (x + y)
+      {
+      case 56:
+        break;
+      case 57:
+        break;
+      case 48:
+        break;
+      case 62:
+        break;
+      case 63:
+        break;
+      case 55:
+        break;
+      case 15:
+        break;
+      case 7:
+        break;
+      case 6:
+        break;
+      case 8:
+        break;
+      case 0:
+        break;
+      case 1:
+        break;
+      default:
+        if (measurementData.distance_mm[x + y] < 300)
+        {
+          Serial.println("Object detected");
+          delay(200);
+          return true;
+        }
+        break;
+      }
+    }
+  }
+  Serial.println("No object detected");
+  delay(200);
+  return false;
+}
+
+//
+//
+// Checks to see if an object has come within the min detect distance
+int objectAvoidance()
+{
+  Serial.println("In object avoidance");
+  delay(200);
+  // Sets direction to undecided(0)
+  which_way = 0;
+
+  // Logic for zone 1 & 3
+  //Serial.println("logic for zone 1 & 3");
+  //delay(500);
+
+  /*Serial.println("zone1locations");
+  for (int a = 0; a <= 23; a++)
+  {
+    Serial.print(measurementData.distance_mm[zone1Locations[a]]);
+    Serial.print("   ");
+  }
+
+  Serial.println("\n");
+  Serial.println("zone2locations");
+  for (int d = 0; d <= 15; d++)
+  {
+    Serial.print(measurementData.distance_mm[zone2Locations[d]]);
+    Serial.print("   ");
+  }
+
+  Serial.println("\n");
+  Serial.println("zone3locations");
+  for (int c = 0; c <= 23; c++)
+  {
+    Serial.print(measurementData.distance_mm[zone3Locations[c]]);
+    Serial.print("   ");
+  }
+  */
+
+  for (int i = 0; i <= 23; ++i)
+  {
+    int x = measurementData.distance_mm[zone1Locations[i]];
+    int y = measurementData.distance_mm[zone3Locations[i]];
+
+    if (x < minDistance)
+    {
+      zone1Values[i] = 1;
+    }
+    else
+    {
+      zone1Values[i] = 0;
+    }
+
+    if (y < minDistance)
+    {
+      zone3Values[i] = 1;
+    }
+    else
+    {
+      zone3Values[i] = 0;
+    }
+  }
+  Serial.println();
+  //Serial.println("logic zone 2");
+  //delay(500);
+  // Logic for zone 2
+  for (int i = 0; i <= 15; ++i)
+  {
+    int z = measurementData.distance_mm[zone2Locations[i]];
+
+    if (z < minDistance)
+    {
+      zone2Values[i] = 1;
+    }
+    else
+    {
+      zone2Values[i] = 0;
+    }
+  }
+  
+  // Logic to calculate percentages
+  double zone1Total = 0;
+  double zone2Total = 0;
+  double zone3Total = 0;
+
+  for (int i = 0; i <= 23; i++)
+  {
+    zone1Total = zone1Total + zone1Values[i];
+    zone3Total = zone3Total + zone3Values[i];
+  }
+
+  for (int i = 0; i <= 15; i++)
+  {
+    zone2Total = zone2Total = zone2Values[i];
+  }
+  /*
+   Serial.print(zone1Total);
+    Serial.print("   ");
+    Serial.print(zone2Total);
+    Serial.print("   ");
+    Serial.println(zone3Total);
+    */
+
+  Serial.println("compare percentages");
+  //delay(500);
+  // Logic get percetages and compare them
+  double z1P = (zone1Total / 24) * 100;
+  double z2P = (zone2Total / 16) * 100;
+  double z3P = (zone3Total / 24) * 100;
+
+  Serial.print(z1P);
+  Serial.print("   ");
+  Serial.print(z2P);
+  Serial.print("   ");
+  Serial.println(z3P);
+
+  if ((z1P <= z2P) && (z1P <= z3P))
+  {
+    // Set direction to left
+    Serial.println("left");
+    return 1;
+  }
+  else if ((z2P <= z1P) && (z2P <= z3P))
+  {
+    // Set direction to forward
+    Serial.println("Firward");
+    return 2;
+  }
+  else if ((z3P <= z1P) && (z3P <= z2P))
+  {
+    // Set direction to right
+    Serial.println("right");
+    return 3;
+  }
+  //Serial.println("end object avoidance");
+  //delay(500);
+  return 0;
+}
+
+//
+//
+//
+
+void get_turn_info()
+{
+  // Variables
+  int direction;
+  bool detection;
+  // put your main code here, to run repeatedly:
+  //delay(1000);
+
+  // Prime sensor to gather good data
+  remindTheSensorItExists();
+
+  // Detect if the sensor has data that can be collected and checks the data for any objects within range.
+  if (dataProcessed() == true)
+  {
+    Serial.println("Data Ready!");
+    //delay(1000);
+
+    // Runs objectDetection algorithm
+    Serial.println("Running Object detection");
+    detection = objectDetection();
+    //delay(500);
+    // remindTheSensorItExists();
+    // delay(500);
+    pitchServo.write(5);
+    //delay(500);
+
+    // If object was detected starts object avoidance
+    if (detection == true)
+    {
+      Serial.println("Object was detected!");
+      //Serial.println("Deciding which direction to move...");
+
+      // Runs objectAvoidance algorithm
+      which_way = objectAvoidance();
+      //Serial.println(which_way);
+      if (which_way == 1)
+      {
+        receivedChars = -90;
+        Serial.println("Direction set to left");
+      }
+      else if (which_way == 2)
+      {
+        receivedChars = 0;
+        Serial.println("Direction set to forward");
+      }
+      else if (which_way == 3)
+      {
+        receivedChars = 90;
+        Serial.println("Direction set to right");
+      }
+      else
+      {
+        Reverse(1000);
+        Serial.println("Direction couldn't be decided upon.");
+        // reverse a little bit and try again.
+      }
+    }
+  }
 }
